f0568852045124b9282c9c60a3dc8af0dd4d0c7a
Bruno Lucattelli <lucattelli@gmail.com>
2022-12-20 11:02:02 -0300

    Move API out of infrastructure

--- src/api/controllers/controllers.module.ts
+++ src/api/controllers/controllers.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CreateUpcomingPolicyCommandHandler } from 'src/application/commands/create-upcoming-policy-command';
-import { PolicyRepositoryPostgressAdapter } from '../adapters/policy-repository-postgres-adapter';
+import { PolicyRepositoryPostgressAdapter } from 'src/infrastructure/adapters/policy-repository-postgres-adapter';
 import { PolicyCommandController } from './policy-command.controller';
 
 const appServiceProvider = {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ControllersModule } from './infrastructure/controllers/controllers.module';
+import { ControllersModule } from './api/controllers/controllers.module';
 
 @Module({
   imports: [ControllersModule],
